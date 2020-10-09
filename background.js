b32a5ae13afda912053fd5b95bf36691fcd6d7d5
John Bieling <john.bieling@gmx.de>
2020-10-09 17:05:29 +0200

    Making it work

--- background.js
+++ background.js
@@ -5,12 +5,15 @@ browser.notificationbox.onButtonClicked.addListener((id, label) => {
     return true;
   }
 });
+
 browser.notificationbox.onClosed.addListener((id) => {
   console.log(`${id} was closed`);
+  return false;
 });
 
-browser.compose.onBeforeSend.addListener(async (tab, details) => {
-  browser.notificationbox.create({
+
+browser.compose.onBeforeSend.addListener(async (tab, details) => { 
+  await browser.notificationbox.create(tab.windowId, {
     label: "Sample notification",
     priority: browser.notificationbox.PRIORITY_CRITICAL_MEDIUM,
     buttons: [
@@ -24,42 +27,6 @@ browser.compose.onBeforeSend.addListener(async (tab, details) => {
     ]
   });
 
-
-  let body = null;
-  if (details.isPlainText) {
-    body = details.plainTextBody;
-  }
-  else {
-    body = details.body;
-  }
-
-  let template = Handlebars.compile(body, { strict: true, explicitPartialContext: true });
-  let context = { test: "this is a test" };
-  try {
-    body = template(context);
-  }
-  catch (e) {
-    if (e instanceof Handlebars.Exception) {
-      if (/^"[^"]+" not defined in /.test(e.message)) {
-        let name = e.message.match(/^"([^"]+)"/)[1];
-
-        browser.notification.create(`"${name}" is not defined`, "error", '', browser.notification.PRIORITY_CRITICAL_MEDIUM);
-      }
-      else {
-        console.log("Uncaught exception:");
-        console.dir(e);
-      }
-      return { cancel: true }
-    }
-    else {
-      throw e;
-    }
-  }
-
-  if (details.isPlainText) {
-    return { details: { plainTextBody: body } };
-  }
-  else {
-    return { details: { body: body } };
-  }
+  return { cancel: true };
+  
 });
--- notificationbox/implementation.js
+++ notificationbox/implementation.js
@@ -1,54 +1,35 @@
 "use strict";
 
 const { EventEmitter, EventManager, ExtensionAPI } = ExtensionCommon;
+const {Services} = ChromeUtils.import("resource://gre/modules/Services.jsm");
 
 XPCOMUtils.defineLazyServiceGetters(this, {
   UUIDGen: ["@mozilla.org/uuid-generator;1", "nsIUUIDGenerator"],
 });
 
 function uuid() {
-  return UUIDGen.generateUUID().toString();
-}
-
-function notificationBox() {
-  let w = Services.wm.getMostRecentWindow(null);
-  if (w.gMessageNotificationBar) {
-    return w.gMessageNotificationBar.msgNotificationBar;
-  }
-  else if (w.specialTabs) {
-    return wspecialTabs.msgNotificationBar;
-  }
-  else if (w.gNotification) {
-    return w.gNotification.notificationbox;
-  }
-  else {
-    throw new Error("Can't find a notification bar");
-  }
+  return `notificationbox-${UUIDGen.generateUUID().number.substring(1, 37)}`;
 }
 
 class Notification {
-  constructor(context, notificationsMap, id, options) {
-    this.notificationsMap = notificationsMap;
-    this.id = id;
+  constructor(windowId, notificationId, options, parent) {
+    this.windowId = windowId;
+    this.notificationId = notificationId;
     this.options = options;
-
-    let imageURL;
-    if (options.iconUrl) {
-      imageURL = context.extension.baseURI.resolve(options.image);
-    }
-
-    // Set before calling into notificationbox, because the notification may be
-    // closed during the call.
-    notificationsMap.set(id, this);
-    var self = this;
+    this.parent = parent;
+    this.imageURL = options.iconUrl
+      ? parent.extension.baseURI.resolve(options.image)
+      : null;
+   
+    let self = this;
     let buttons = options.buttons.map(function(button) {
       return {
         label: button.label,
         accesskey: button.accesskey,
-        callback: function(/*notificationBox, buttonDescription, eventTarget*/) {
-          // Fire the event and sort out if we need to close the notification
-          // later.
-          self.notificationsMap.emitter.emit("buttonclicked", self.id, button.label).then((values) => {
+        callback: function() {
+          // Fire the event and keep the notification open, decided to close it
+          // based on the return values later.
+          self.parent.emitter.emit("buttonclicked", self.notificationId, button.label).then((values) => {
             let allTrue = values.every((value) => value === true);
             if (!allTrue) {
               self.clear();
@@ -63,41 +44,44 @@ class Notification {
     });
 
     let callback = function(event) {
+      console.log(`Notification event (${self.notificationId}: ${event}`);
       if (event === "removed") {
-        self.notificationsMap.emitter.emit("closed", self.id)
-      }
-      else {
-        console.log(`Notification event (${self.id}: ${event}`);
+        self.parent.emitter.emit("closed", self.notificationId, /* closed by user */ true);
       }
     };
 
-    try {
-      notificationBox().appendNotification(options.label, this.id, imageURL, this.options.priority, buttons, callback);
-    } catch (e) {
-      // This will fail if notificationbox is not available.
-    }
+    this.getNotificationBox().appendNotification(this.options.label, this.notificationId, this.imageURL, this.options.priority, buttons, callback);
   }
 
-  clear() {
-    try {
-      notificationBox().removeNotification(
-        notificationBox().getNotificationWithValue(this.id)
-      );
-    } catch (e) {
-      // FIXME handle the error
+  getNotificationBox() {
+    let w = this.parent.extension.windowManager.get(this.windowId, this.parent.context).window;
+    if (w.gMessageNotificationBar) {
+      return w.gMessageNotificationBar.msgNotificationBar;
     }
-    this.notificationsMap.delete(this.id);
+    else if (w.specialTabs) {
+      return wspecialTabs.msgNotificationBar;
+    }
+    else if (w.gNotification) {
+      return w.gNotification.notificationbox;
+    }
+    else {
+      throw new Error("Can't find a notification bar");
+    }
+  }
+  
+  clear() {
+      let notificationBox = this.getNotificationBox();
+      let notification = notificationBox.getNotificationWithValue(this.notificationId);
+      notificationBox.removeNotification(notification);
+      this.parent.notificationsMap.delete(this.notificationId);    
   }
 }
 
 var notificationbox = class extends ExtensionAPI {
   constructor(extension) {
     super(extension);
-
-    this.idCounter = 0;
     this.notificationsMap = new Map();
     this.emitter = new EventEmitter();
-    this.notificationsMap.emitter = this.emitter;
   }
 
   onShutdown() {
@@ -107,32 +91,29 @@ var notificationbox = class extends ExtensionAPI {
   }
 
   getAPI(context) {
-    let notificationsMap = this.notificationsMap;
-    let emitter = this.emitter;
+    this.context = context;
+    let self = this
 
     return {
       notificationbox: {
-        create(notificationId, options) {
+        async create(windowId, notificationId, options) {
           if (!notificationId) {
-            let newId = `notification-${uuid()}`;
-            while (notificationsMap.has(newId)) {
-              newId = `notification-${uuid()}`;
-            }
-            notificationId = newId;
+            do {
+              notificationId =uuid();
+            } while (self.notificationsMap.has(notificationId))
           }
 
-          if (notificationsMap.has(notificationId)) {
-            notificationsMap.get(notificationId).clear();
+          if (self.notificationsMap.has(notificationId)) {
+            self.notificationsMap.get(notificationId).clear();
           }
 
-          new Notification(context, notificationsMap, notificationId, options);
-
-          return Promise.resolve(notificationId);
+          self.notificationsMap.set(notificationId, new Notification(windowId, notificationId, options, self));
+          return notificationId;
         },
 
         clear: function(notificationId) {
-          if (notificationsMap.has(notificationId)) {
-            notificationsMap.get(notificationId).clear();
+          if (self.notificationsMap.has(notificationId)) {
+            self.notificationsMap.get(notificationId).clear();
             return true;
           }
           return false;
@@ -140,7 +121,7 @@ var notificationbox = class extends ExtensionAPI {
 
         getAll: function() {
           let result = {};
-          notificationsMap.forEach((value, key) => {
+          self.notificationsMap.forEach((value, key) => {
             result[key] = value.options;
           });
           return result;
@@ -150,14 +131,13 @@ var notificationbox = class extends ExtensionAPI {
           context,
           name: "notificationbox.onClosed",
           register: fire => {
-            let listener = (event, notificationId) => {
-              // TODO Bug 1413188, Support the byUser argument.
-              fire.async(notificationId, true);
+            let listener = (event, notificationId, closedByUser) => {
+              fire.async(notificationId, closedByUser);
             };
 
-            emitter.on("closed", listener);
+            self.emitter.on("closed", listener);
             return () => {
-              emitter.off("closed", listener);
+              self.emitter.off("closed", listener);
             };
           },
         }).api(),
@@ -170,9 +150,9 @@ var notificationbox = class extends ExtensionAPI {
               return fire.async(notificationId, label);
             };
 
-            emitter.on("buttonclicked", listener);
+            self.emitter.on("buttonclicked", listener);
             return () => {
-              emitter.off("buttonclicked", listener);
+              self.emitter.off("buttonclicked", listener);
             };
           },
         }).api(),
