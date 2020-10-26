f2b4fc5e89e73480e6a06b32222907f03f145c6c
John Bieling <john.bieling@gmx.de>
2020-10-26 09:59:15 +0100

    Check if style is used at all

--- notificationbox/implementation.js
+++ notificationbox/implementation.js
@@ -52,19 +52,21 @@ class Notification {
     let element = this.getNotificationBox().appendNotification(options.label, `extension-notification-${notificationId}`, imageURL, options.priority, buttons, callback);
     let whitelist = ["background", "color", "margin", "padding", "font"];
 
-    let sanitizedStyles = Object.keys(options.style).filter(style => {
-      let parts = style.split("-");
-      return (
-        // check if first part is in whitelist
-        parts.length > 0 && 
-        whitelist.includes(parts[0]) &&
-        // validate second part (if any) being a simple word
-        (parts.length == 1 || (parts.length == 2 && /^[a-zA-Z0-9]+$/.test(parts[1])))
-      );
-    });   
-    
-    for (let style of sanitizedStyles) {
-      element.style[style] = options.style[style];
+    if (options.style) {
+      let sanitizedStyles = Object.keys(options.style).filter(style => {
+        let parts = style.split("-");
+        return (
+          // check if first part is in whitelist
+          parts.length > 0 && 
+          whitelist.includes(parts[0]) &&
+          // validate second part (if any) being a simple word
+          (parts.length == 1 || (parts.length == 2 && /^[a-zA-Z0-9]+$/.test(parts[1])))
+        );
+      });   
+      
+      for (let style of sanitizedStyles) {
+        element.style[style] = options.style[style];
+      }
     }
   }
 
