3c0bd6cdcdd86a5e9f521ff7f5562edcf7a2dffd
John Bieling <john.bieling@gmx.de>
2020-10-27 16:00:17 +0100

    browser -> messenger

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 // Defining a onButtonClicked listener
-browser.notificationbox.onButtonClicked.addListener((windowId, notificationId, buttonId) => {
+messenger.notificationbox.onButtonClicked.addListener((windowId, notificationId, buttonId) => {
   console.log(`Listener #1 sees: ${buttonId} clicked in box ${notificationId} in window ${windowId}.`);
     if (["btn-keep"].includes(buttonId)) {
     console.log("Box will not close, as long as one listener returns {close:false}.");
@@ -8,7 +8,7 @@ browser.notificationbox.onButtonClicked.addListener((windowId, notificationId, b
 });
 
 // Defining another onButtonClicked listener
-browser.notificationbox.onButtonClicked.addListener((windowId, notificationId, buttonId) => {
+messenger.notificationbox.onButtonClicked.addListener((windowId, notificationId, buttonId) => {
   console.log(`Listener #2 sees: ${buttonId} clicked in box ${notificationId} in window ${windowId}.`);
   if (["btn-direct"].includes(buttonId)) {
     console.log("Box will close as long no listener returns {close:false}.");
@@ -16,12 +16,12 @@ browser.notificationbox.onButtonClicked.addListener((windowId, notificationId, b
 });
 
 // Defining a onDismissed listener
-browser.notificationbox.onDismissed.addListener((windowId, notificationId) => {
+messenger.notificationbox.onDismissed.addListener((windowId, notificationId) => {
   console.log(`${notificationId} in window ${windowId} was dismissed`);
 });
 
 // Defining a onClosed listener
-browser.notificationbox.onClosed.addListener((windowId, notificationId, closedByUser) => {
+messenger.notificationbox.onClosed.addListener((windowId, notificationId, closedByUser) => {
   console.log(`${notificationId} in window ${windowId} was closed by user: ${closedByUser}`);
 });
 
@@ -29,11 +29,11 @@ browser.notificationbox.onClosed.addListener((windowId, notificationId, closedBy
 
 async function addBoxes(window) {
   // adding a top box
-  await browser.notificationbox.create({
+  await messenger.notificationbox.create({
     windowId: window.id,
     label: "Sample notification top 1",
     placement: "top",
-    priority: browser.notificationbox.PRIORITY_WARNING_HIGH,
+    priority: messenger.notificationbox.PRIORITY_WARNING_HIGH,
     buttons: [
       {
         id: "btn-direct",
@@ -48,17 +48,15 @@ async function addBoxes(window) {
   });
   
   // some default boxes to test stacking
-  await browser.notificationbox.create({
+  await messenger.notificationbox.create({
     windowId: window.id,
     label: "Sample notification default 1",
-    placement: "default",    
   });
 
-  await browser.notificationbox.create({
+  await messenger.notificationbox.create({
     windowId: window.id,
-    priority: browser.notificationbox.PRIORITY_WARNING_HIGH,
+    priority: messenger.notificationbox.PRIORITY_CRITICAL_HIGH,
     label: "Sample notification default 2",
-    placement: "default",    
     buttons: [
       {
         id: "btn",
@@ -68,33 +66,30 @@ async function addBoxes(window) {
     ]
   });
 
- await browser.notificationbox.create({
-    windowId: window.id,
-    label: "Sample notification default 3",
-    placement: "default",    
-  });
-
-
   // a bottom box
-  await browser.notificationbox.create({
+  await messenger.notificationbox.create({
     windowId: window.id,
     label: "Sample notification bottom 1",
     image: "icon.png",
     placement: "bottom",    
   });
-  
-  console.log(await browser.notificationbox.getAll());
 }
 
 
 
 // add boxes to all future windows
-browser.windows.onCreated.addListener(addBoxes);
+messenger.windows.onCreated.addListener(addBoxes);
 
 // add boxes to all existing windows
-browser.windows.getAll()
+messenger.windows.getAll()
   .then(windows => {
     for (let window of windows) {
       addBoxes(window);
     }
   });
+  
+  messenger.windows.create({
+                height: 200,
+                width: 510,
+                type: "popup"
+            })
