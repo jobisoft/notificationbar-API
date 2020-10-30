eae17038282b2436a637c9372cc57a3f5a46780e
John Bieling <john.bieling@gmx.de>
2020-10-30 12:34:37 +0100

    eslint --fix

--- notificationbar/implementation.js
+++ notificationbar/implementation.js
@@ -11,45 +11,69 @@ class Notification {
     this.properties = properties;
     this.parent = parent;
 
-    let iconURL = (properties.icon && !properties.icon.includes(":"))
-      ? parent.extension.baseURI.resolve(properties.icon)
-      : null;
-   
+    let iconURL =
+      properties.icon && !properties.icon.includes(":")
+        ? parent.extension.baseURI.resolve(properties.icon)
+        : null;
+
     let self = this;
     let buttons = properties.buttons.map(function(button) {
       return {
         id: button.id,
         label: button.label,
         accesskey: button.accesskey,
-        callback: function() {
+        callback() {
           // Fire the event and keep the notification open, decided to close it
           // based on the return values later.
-          self.parent.emitter.emit("buttonclicked", self.properties.windowId, self.notificationId, button.id).then((rv) => {
-            let keepOpen = rv.some((value) => value?.close === false);            
-            if (!keepOpen) {
-              self.remove(/* closedByUser */ true);
-            }
-          });
+          self.parent.emitter
+            .emit(
+              "buttonclicked",
+              self.properties.windowId,
+              self.notificationId,
+              button.id
+            )
+            .then(rv => {
+              let keepOpen = rv.some(value => value?.close === false);
+              if (!keepOpen) {
+                self.remove(/* closedByUser */ true);
+              }
+            });
 
           // Keep the notification box open until we hear from the event
           // handlers.
           return true;
-        }
-      }
+        },
+      };
     });
 
     let callback = function(event) {
       // Every dismissed notification will also generate a removed notification
       if (event === "dismissed") {
-        self.parent.emitter.emit("dismissed", self.properties.windowId, self.notificationId);
+        self.parent.emitter.emit(
+          "dismissed",
+          self.properties.windowId,
+          self.notificationId
+        );
       }
       if (event === "removed") {
-        self.parent.emitter.emit("closed", self.properties.windowId, self.notificationId, self.closedByUser);
+        self.parent.emitter.emit(
+          "closed",
+          self.properties.windowId,
+          self.notificationId,
+          self.closedByUser
+        );
         self.cleanup();
       }
     };
 
-    let element = this.getNotificationBox().appendNotification(properties.label, `extension-notification-${notificationId}`, iconURL, properties.priority, buttons, callback);
+    let element = this.getNotificationBox().appendNotification(
+      properties.label,
+      `extension-notification-${notificationId}`,
+      iconURL,
+      properties.priority,
+      buttons,
+      callback
+    );
     let whitelist = ["background", "color", "margin", "padding", "font"];
 
     if (properties.style) {
@@ -57,13 +81,14 @@ class Notification {
         let parts = style.split("-");
         return (
           // check if first part is in whitelist
-          parts.length > 0 && 
+          parts.length > 0 &&
           whitelist.includes(parts[0]) &&
           // validate second part (if any) being a simple word
-          (parts.length == 1 || (parts.length == 2 && /^[a-zA-Z0-9]+$/.test(parts[1])))
+          (parts.length == 1 ||
+            (parts.length == 2 && /^[a-zA-Z0-9]+$/.test(parts[1])))
         );
-      });   
-      
+      });
+
       for (let style of sanitizedStyles) {
         element.style[style] = properties.style[style];
       }
@@ -71,16 +96,18 @@ class Notification {
   }
 
   getNotificationBox() {
-    let w = this.parent.extension.windowManager.get(this.properties.windowId, this.parent.context).window;
+    let w = this.parent.extension.windowManager.get(
+      this.properties.windowId,
+      this.parent.context
+    ).window;
     switch (this.properties.placement) {
-      case "message":
-        {
-          // below the receipient list in the message preview window
-          if (w.gMessageNotificationBar) {
-            return w.gMessageNotificationBar.msgNotificationBar;
-          }
+      case "message": {
+        // below the receipient list in the message preview window
+        if (w.gMessageNotificationBar) {
+          return w.gMessageNotificationBar.msgNotificationBar;
         }
-        
+      }
+
       case "bottom":
         {
           // default bottom notification in the mail3:pane
@@ -94,38 +121,52 @@ class Notification {
           }
           // if there is no default bottom box, use our own
           if (!w.gExtensionNotificationBottomBox) {
-            let statusbar = w.document.querySelector('hbox[class~="statusbar"]');
-            w.gExtensionNotificationBottomBox = new w.MozElements.NotificationBox(element => {
-              element.id = "extension-notification-bottom-box";
-              element.setAttribute("notificationside", "bottom");
-              if (statusbar) {
-                w.document.documentElement.insertBefore(element, statusbar);                
-              } else {
-                w.document.documentElement.append(element);
+            let statusbar = w.document.querySelector(
+              'hbox[class~="statusbar"]'
+            );
+            w.gExtensionNotificationBottomBox = new w.MozElements.NotificationBox(
+              element => {
+                element.id = "extension-notification-bottom-box";
+                element.setAttribute("notificationside", "bottom");
+                if (statusbar) {
+                  w.document.documentElement.insertBefore(element, statusbar);
+                } else {
+                  w.document.documentElement.append(element);
+                }
               }
-            });
-          }            
+            );
+          }
           return w.gExtensionNotificationBottomBox;
         }
         break;
-        
+
       case "top":
         {
           if (!w.gExtensionNotificationTopBox) {
             // try to add it before the toolbox, if that fails add it firstmost
             let toolbox = w.document.querySelector("toolbox");
             if (toolbox) {
-              w.gExtensionNotificationTopBox = new w.MozElements.NotificationBox(element => {
-                element.id = "extension-notification-top-box";
-                element.setAttribute("notificationside", "top");
-                toolbox.parentElement.insertBefore(element, toolbox.nextElementSibling);
-              });
-            } else {              
-              w.gExtensionNotificationTopBox = new w.MozElements.NotificationBox(element => {
-                element.id = "extension-notification-top-box";
-                element.setAttribute("notificationside", "top");
-                w.document.documentElement.insertBefore(element, w.document.documentElement.firstChild);                
-              });              
+              w.gExtensionNotificationTopBox = new w.MozElements.NotificationBox(
+                element => {
+                  element.id = "extension-notification-top-box";
+                  element.setAttribute("notificationside", "top");
+                  toolbox.parentElement.insertBefore(
+                    element,
+                    toolbox.nextElementSibling
+                  );
+                }
+              );
+            } else {
+              w.gExtensionNotificationTopBox = new w.MozElements.NotificationBox(
+                element => {
+                  element.id = "extension-notification-top-box";
+                  element.setAttribute("notificationside", "top");
+                  w.document.documentElement.insertBefore(
+                    element,
+                    w.document.documentElement.firstChild
+                  );
+                }
+              );
             }
           }
           return w.gExtensionNotificationTopBox;
@@ -133,19 +174,21 @@ class Notification {
         break;
     }
   }
-  
+
   remove(closedByUser) {
-      // The remove() method is called by button clicks and by notificationBox.clear()
-      // but not by dismissal. In that case, the default value defined in the constructor
-      // defines the value of closedByUser which is used by the event emitter.
-      this.closedByUser = closedByUser;
-      let notificationBox = this.getNotificationBox();
-      let notification = notificationBox.getNotificationWithValue(`extension-notification-${this.notificationId}`);
-      notificationBox.removeNotification(notification);
+    // The remove() method is called by button clicks and by notificationBox.clear()
+    // but not by dismissal. In that case, the default value defined in the constructor
+    // defines the value of closedByUser which is used by the event emitter.
+    this.closedByUser = closedByUser;
+    let notificationBox = this.getNotificationBox();
+    let notification = notificationBox.getNotificationWithValue(
+      `extension-notification-${this.notificationId}`
+    );
+    notificationBox.removeNotification(notification);
   }
 
   cleanup() {
-      this.parent.notificationsMap.delete(this.notificationId);    
+    this.parent.notificationsMap.delete(this.notificationId);
   }
 }
 
@@ -155,7 +198,7 @@ var notificationbar = class extends ExtensionAPI {
     this.notificationsMap = new Map();
     this.emitter = new EventEmitter();
     this.nextId = 1;
-    Services.obs.addObserver(this, "domwindowclosed", false);
+    Services.obs.addObserver(this, "domwindowclosed");
   }
 
   onShutdown() {
@@ -167,7 +210,7 @@ var notificationbar = class extends ExtensionAPI {
 
   // Observer for the domwindowclosed notification, to remove
   // obsolete notifications from the notificationsMap.
-  observe(aSubject, aTopic, aData) {    
+  observe(aSubject, aTopic, aData) {
     let win = this.context.extension.windowManager.convert(aSubject);
     this.notificationsMap.forEach((value, key) => {
       if (value.properties.windowId == win.id) {
@@ -175,8 +218,7 @@ var notificationbar = class extends ExtensionAPI {
       }
     });
   }
-  
- 
+
   getAPI(context) {
     this.context = context;
     let self = this;
@@ -185,13 +227,18 @@ var notificationbar = class extends ExtensionAPI {
       notificationbar: {
         async create(properties) {
           let notificationId = self.nextId++;
-          self.notificationsMap.set(notificationId, new Notification(notificationId, properties, self));
+          self.notificationsMap.set(
+            notificationId,
+            new Notification(notificationId, properties, self)
+          );
           return notificationId;
         },
-        
+
         async clear(notificationId) {
           if (self.notificationsMap.has(notificationId)) {
-            self.notificationsMap.get(notificationId).remove(/* closedByUser */ false);
+            self.notificationsMap
+              .get(notificationId)
+              .remove(/* closedByUser */ false);
             return true;
           }
           return false;
@@ -249,7 +296,7 @@ var notificationbar = class extends ExtensionAPI {
             };
           },
         }).api(),
-      }
-    }
+      },
+    };
   }
-}
+};
